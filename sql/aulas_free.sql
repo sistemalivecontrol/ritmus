a7f7ce57be0f1c1743110c65160ec5eba31e9327
sistemalivecontrol <marcobett@gmail.com>
2026-06-16 09:18:00 -0300

    Delete sql directory

--- sql/aulas_free.sql
+++
@@ -1,6 +0,0 @@
--- Adicionar campo gratuito na tabela aulas
-ALTER TABLE public.aulas 
-ADD COLUMN IF NOT EXISTS gratuito BOOLEAN DEFAULT false;
-
--- Atualizar aulas existentes (opcional)
--- UPDATE public.aulas SET gratuito = false WHERE gratuito IS NULL;
